588bd8cd3f75168a5d5494548ec9e1574d370391
Petrik Diak <diak@petrik.hu>
2026-01-14 09:11:05 +0100

    L

--- 0107/MainWindow.xaml.cs
+++ 0107/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 using System.Windows.Threading;
 
 namespace _0107
@@ -17,14 +18,33 @@ namespace _0107
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        double score;
+        int gravity = 8;
+        bool gameOver;
+        Rect flappyBirdHiBox;
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMicroseconds(20);
+            StartGame();
+
         }
+
+		private void MainEventTimer(object sender, EventArgs e)
+		{
+			
+		}
 
 		private void KeyIsDown(object sender, KeyEventArgs e)
 		{
-
+            if (e.Key == Key.Space)
+            {
+                madar.RenderTransform = new RotateTransform(-20, madar.Width /2, madar.Height /2);
+            }
 		}
 
 		private void KeyIsUp(object sender, KeyEventArgs e)
@@ -34,8 +54,35 @@ namespace _0107
 
         private void StartGame()
         {
+            MyCanvas.Focus();
+
+            int temp = 300;
+
+            score = 0;
+
+            gameOver = false;
+            Canvas.SetTop(madar, 190);
+
+            foreach (var x in MyCanvas.Children.OfType<Image>())
+            { 
+                if((string)x.Tag == "oszlop1")
+                {
+                    Canvas.SetLeft(x, 500);
+                }
+				if ((string)x.Tag == "oszlop2")
+				{
+					Canvas.SetLeft(x, 800);
+				}
+				if ((string)x.Tag == "oszlop3")
+				{
+					Canvas.SetLeft(x, 1100);
+				}
+			}
+
+            gameTimer.Start();
 
         }
+
 
         private void EndGame()
         {
